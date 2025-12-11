66961e4596c9756bcc6527fea1721a40e2512bd6
demonte17 <17demonte17@gmail.com>
2025-12-10 20:36:12 -0800

    second commit

--- 5-rtb.tf
+++ 5-rtb.tf
@@ -34,7 +34,7 @@ resource "aws_route_table" "rtb1private" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
   }
 
   tags = {
